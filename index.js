bf911e49eb76e954c1567ee0026cc6b657912278
anhhoangtn93 <131422999+anhhoangtn93@users.noreply.github.com>
2024-01-29 21:37:48 +0700

    add cart button

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     nav: true,
     dots: false,
     autoplayTimeout: 8000,
-    autoplay:true
+    autoplay: true,
   });
   $(".owl-carousel.brand").owlCarousel({
     loop: true,
@@ -20,7 +20,16 @@ $(document).ready(function () {
     nav: true,
     dots: false,
     autoplayTimeout: 3000,
-    autoplay:true
+    autoplay: true,
+  });
+  $(".owl-carousel.trending").owlCarousel({
+    loop: true,
+    navigation: true,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    items: 4,
+    nav: true,
+    dots: false,
   });
   $(".dropdown").on("click", function () {
     var dropdownContent = $(this).find(".dropdown-content");
@@ -37,26 +46,8 @@ $(document).ready(function () {
       }
     });
   });
-  //slider
-  let slideIndex = 0;
-  showSlides();
-
-  function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-  }
+  //close promo
+  $(".wrap-promo .close").click(() => {
+    $(".wrap-promo").hide();
+  });
 });
